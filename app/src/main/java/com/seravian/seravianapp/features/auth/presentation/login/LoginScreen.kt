af4ce7d16ec1fd0ca323e1cff8428423459f22c8
GreenVenom77 <hosome2222@gmail.com>
2024-12-07 15:42:21 +0200

    Added a new background for auth screens and done the top part of the register screen

--- app/src/main/java/com/seravian/seravianapp/features/auth/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/seravian/seravianapp/features/auth/presentation/login/LoginScreen.kt
@@ -3,6 +3,8 @@ package com.seravian.seravianapp.features.auth.presentation.login
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
@@ -15,9 +17,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,54 +57,75 @@ fun LoginContents(
     action: (LoginAction) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = Modifier
-            .background(color = AuthBlackColor)
-            .fillMaxWidth()
-            .fillMaxHeight(.35f)
-            .padding(20.dp)
-            .windowInsetsPadding(WindowInsets.safeDrawing)
-    ) {
-        Row {
+    Column() {
+        Box(
+            modifier = Modifier
+                .background(color = AuthBlackColor)
+                .fillMaxWidth()
+                .fillMaxHeight(0.34f)
+        ) {
             Image(
-                painter = painterResource(R.drawable.logo),
-                contentDescription = stringResource(R.string.logo_image),
-                modifier = Modifier.padding(5.dp)
-            )
-            Text(
-                text = stringResource(R.string.seravian),
-                color = colorResource(R.color.white),
-                modifier = Modifier.padding(5.dp)
-            )
-        }
-        Text(
-            text = stringResource(R.string.sign_in_into_your_account),
-            style = MaterialTheme.typography.bodyLarge.copy(
-                fontSize = 30.sp, // Override the font size if necessary
-                lineHeight = 40.sp // Add desired line height
-            ),
-            color = colorResource(R.color.white),
-            modifier = Modifier.padding(top = 30.dp, start = 5.dp)
-        )
-
-        Row(modifier = Modifier.padding(top = 20.dp)) {
-            Text(
-                text = stringResource(R.string.don_t_have_an_account),
-                color = colorResource(R.color.white),
-
-            )
-            Text(
-                text = stringResource(R.string.sign_up),
-                color = bluePrimary,
-                style = MaterialTheme.typography.bodyLarge.copy(
-                    textDecoration = TextDecoration.Underline
-                ),
+                painter = painterResource(id = R.drawable.star),
+                contentDescription = "Stars",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .padding(start = 3.dp)
-                    .clickable(enabled = true) {
-                        appNavigator?.navigateTo(AppDestination.Register)
-                    }
+                    .fillMaxWidth()
+                    .fillMaxHeight()
             )
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(horizontal = 20.dp)
+                    .windowInsetsPadding(WindowInsets.safeDrawing)
+            ) {
+                Row {
+                    Image(
+                        painter = painterResource(R.drawable.logo),
+                        contentDescription = stringResource(R.string.logo_image),
+                        modifier = Modifier.padding(5.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.seravian),
+                        color = colorResource(R.color.white),
+                        modifier = Modifier.padding(5.dp)
+                    )
+                }
+                Text(
+                    text = stringResource(R.string.sign_in_into_your_account),
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontSize = 30.sp,
+                        lineHeight = 40.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    color = colorResource(R.color.white),
+                    modifier = Modifier.padding(top = 30.dp, start = 5.dp)
+                )
+
+                Row(modifier = Modifier.padding(top = 20.dp)) {
+                    Text(
+                        text = stringResource(R.string.don_t_have_an_account),
+                        color = colorResource(R.color.white),
+
+                        )
+                    Text(
+                        text = stringResource(R.string.sign_up),
+                        color = bluePrimary,
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            textDecoration = TextDecoration.Underline
+                        ),
+                        modifier = Modifier
+                            .padding(start = 3.dp)
+                            .clickable(enabled = true) {
+                                appNavigator?.navigateTo(AppDestination.Register)
+                            }
+                    )
+                }
+            }
+        }
+        Column() {
+
         }
     }
 }
--- app/src/main/java/com/seravian/seravianapp/features/auth/presentation/register/RegisterScreen.kt
+++ app/src/main/java/com/seravian/seravianapp/features/auth/presentation/register/RegisterScreen.kt
@@ -1,11 +1,41 @@
 package com.seravian.seravianapp.features.auth.presentation.register
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.seravian.seravianapp.R
+import com.seravian.seravianapp.core.navigation.AppDestination
 import com.seravian.seravianapp.core.navigation.AppNavigator
 import com.seravian.seravianapp.core.presentation.BaseScreen
+import com.seravian.seravianapp.ui.theme.AuthBlackColor
 import com.seravian.seravianapp.ui.theme.SeravianTheme
+import com.seravian.seravianapp.ui.theme.bluePrimary
 
 @Composable
 fun RegisterScreen(
@@ -13,19 +43,105 @@ fun RegisterScreen(
     modifier: Modifier = Modifier
 ) {
     BaseScreen<RegisterViewModel> { viewModel ->
-        RegisterContents()
+        val state = viewModel.state
+        RegisterContents(
+            appNavigator = appNavigator,
+            state = state,
+            action = viewModel::onAction
+        )
     }
 }
 
 @Composable
-fun RegisterContents(modifier: Modifier = Modifier) {
+fun RegisterContents(
+    appNavigator: AppNavigator?,
+    state: RegisterInputState,
+    action: (RegisterAction) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column() {
+        Box(
+            modifier = Modifier
+                .background(color = AuthBlackColor)
+                .fillMaxWidth()
+                .fillMaxHeight(0.34f)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.star),
+                contentDescription = "Stars",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            )
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(horizontal = 20.dp)
+                    .windowInsetsPadding(WindowInsets.safeDrawing)
+            ) {
+                IconButton(onClick = { appNavigator?.navigateBack() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.white_arrow_back_2),
+                        contentDescription = "Back Button",
+                        tint = colorResource(R.color.white),
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                Text(
+                    text = stringResource(R.string.register),
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontSize = 50.sp,
+                        lineHeight = 40.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    color = colorResource(R.color.white),
+                    modifier = Modifier.padding(top = 30.dp, start = 5.dp)
+                )
+                Row(modifier = Modifier.padding(top = 20.dp)) {
+                    Text(
+                        text = stringResource(R.string.already_have_an_account),
+                        color = colorResource(R.color.white),
 
+                        )
+                    Text(
+                        text = stringResource(R.string.sign_in),
+                        color = bluePrimary,
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            textDecoration = TextDecoration.Underline
+                        ),
+                        modifier = Modifier
+                            .padding(start = 3.dp)
+                            .clickable(enabled = true) {
+                                appNavigator?.navigateTo(AppDestination.Login)
+                            }
+                    )
+                }
+            }
+        }
+        Column() {
+
+        }
+    }
 }
+
+val mockState = RegisterInputState(
+    usernameValidity = null,
+    emailValidity = null,
+    passwordValidity = null,
+    confirmPasswordValidity = null
+)
 
 @Preview(showSystemUi = true)
 @Composable
 private fun RegisterContentsPreview() {
     SeravianTheme {
-        RegisterContents()
+        RegisterContents(
+            appNavigator = null,
+            state = mockState,
+            action = {  }
+        )
     }
 }
